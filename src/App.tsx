79b3c616e85c404262a19230f04397fbc77371f4
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2024-04-01 22:58:04 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const[users, setUsers] = useState([])
+  
+
+  interface Users {
+    name: string;
+    id: number;
+    username: string;
+    email:string;
+  }
+
+  useEffect(()=> {
+    const fetchData = async () => {
+        try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const results = await response.json()
+        setUsers(results.slice(0,4))
+      
+        console.log(results)
+    }
+    catch (error) {
+      console.log(error)
+    }
+    }
+    fetchData()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Isaac!</h1>
+      <h2>data here</h2>
+      <div>{ users.map((user: Users) =><div key={user.id}><li>{user.name}</li>
+      <li>{user.email}</li>
+
+      </div>)
+        }</div>
     </div>
   );
 }
